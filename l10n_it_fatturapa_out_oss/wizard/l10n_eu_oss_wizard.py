masimassimo/l10n-italy-split-fix

Set N3.2 for OSS taxes

l10n_it_fatturapa_out_oss/wizard/l10n_eu_oss_wizard.py
@@ -1,0 +1,10 @@
+from odoo import models
+
+
+class L10nEuOssWizard(models.TransientModel):
+    _inherit = "l10n.eu.oss.wizard"
+
+    def _prepare_tax_vals(self, country_id, tax_id, rate):
+        vals = super(L10nEuOssWizard, self)._prepare_tax_vals(country_id, tax_id, rate)
+        vals["kind_id"] = self.env.ref("l10n_it_account_tax_kind.n3_2").id
+        return vals

efatto/l10n-italy

l10n_it_fatturapa_out_oss/wizard/l10n_eu_oss_wizard.py
@@ -1,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from openerp import models
+
+
+class L10nEuOssWizard(models.TransientModel):
+    _inherit = "l10n.eu.oss.wizard"
+
+    def _prepare_tax_vals(self, country_id, tax_id, rate):
+        vals = super(L10nEuOssWizard, self)._prepare_tax_vals(country_id, tax_id, rate)
+        vals["kind_id"] = self.env.ref("l10n_it_account_tax_kind.n3_2").id
+        return vals