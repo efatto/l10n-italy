efcaa434e94f71c6001cd9f4f51eccb610cf3c52
sergiocorato <sergiocorato@gmail.com>
2025-02-13 19:22:03 +0100

    [WIP] fix migration for l10n_it_fatturapa_out

--- l10n_it_fatturapa_out/migrations/16.0.1.3.0/pre-migration.py
+++ l10n_it_fatturapa_out/migrations/16.0.1.3.0/pre-migration.py
@@ -13,10 +13,5 @@ def migrate(cr, version):
                 "l10n_it_fatturapa_pec.group_force_e_inv_export_state",
                 "l10n_it_fatturapa_out.group_force_e_inv_export_state",
             ),
-            (
-                "l10n_it_fatturapa_pec"
-                ".view_fatturapa_out_pec_attachment_form_statusbar",
-                "l10n_it_fatturapa_out.fatturapa_attachment_out_form_statusbar",
-            ),
         ],
     )
