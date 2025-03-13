e0025618326381cf662e8e23a9a1a3317630e343
SoSylwe <skrasinski62@gmail.com>
2025-03-13 09:51:59 +0100

    [IMP]l10n_it_ricevute_bancarie : Added new fields in C/O Wizard

--- l10n_it_ricevute_bancarie/tests/test_riba.py
+++ l10n_it_ricevute_bancarie/tests/test_riba.py
@@ -233,7 +233,7 @@ class TestInvoiceDueCost(riba_common.TestRibaCommon):
             .create(
                 {
                     "bank_amount": 455,
-                    "expense_amount": 5,
+                    "past_due_fee_amount": 5,
                 }
             )
         )
@@ -410,7 +410,7 @@ class TestInvoiceDueCost(riba_common.TestRibaCommon):
             .create(
                 {
                     "bank_amount": 102,
-                    "expense_amount": 2,
+                    "past_due_fee_amount": 2,
                 }
             )
         )
@@ -690,3 +690,34 @@ class TestInvoiceDueCost(riba_common.TestRibaCommon):
         self.assertIn("Cannot post invoices", err_msg)
         self.assertIn(self.invoice.partner_id.display_name, err_msg)
         self.assertIn(str(self.invoice.amount_total), err_msg)
+
+    def test_past_due_fee_amount_flow(self):
+        config = self.env["riba.configuration"].create(
+            {
+                "name": "Test Config",
+                "type": "sbf",
+                "bank_id": self.company_bank.id,
+                "past_due_fee_amount": 15.0,
+            }
+        )
+        self.assertEqual(config.past_due_fee_amount, 15.0)
+
+        distinta = self.env["riba.distinta"].create(
+            {
+                "config_id": config.id,
+                "name": "Test Distinta",
+            }
+        )
+        distinta_line = self.env["riba.distinta.line"].create(
+            {"distinta_id": distinta.id, "amount": 100.0}
+        )
+
+        wizard = (
+            self.env["riba.unsolved"]
+            .with_context(
+                active_model="riba.distinta.line",
+                active_id=distinta_line.id,
+            )
+            .create({})
+        )
+        self.assertEqual(wizard.past_due_fee_amount, 15.0)
