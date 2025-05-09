92b9a6c1f1b88371215297e8f827f50db04cb566
eLBati <elbaddy@gmail.com>
2025-05-09 14:53:02 +0200

    [REF] l10n_it_account - Adding _get_credit_accounts and _get_debit_accounts - Compute deductible_balance in case of split payment and comments - Add customer_balance and supplier_balance for VAT documents

--- l10n_it_account/__manifest__.py
+++ l10n_it_account/__manifest__.py
@@ -21,6 +21,7 @@
         "account_tax_balance",
         "date_range",
         "web",
+        "l10n_it",  # for _l10n_it_is_split_payment
     ],
     "data": [
         "views/account_menuitem.xml",
